f742bfb0ed2219182bac6514fcd5c99263961910
vluca997 <vluca997@gmail.com>
2024-06-29 14:18:18 +0200

    grafica

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -13,11 +13,14 @@ const Products = () => {
 
     return (
         <section className='py-16 bg-gray-200'>
-            <div className='container mx-auto flex flex-col md:flex-row'>
-                <div className='w-full md:w-1/4 p-4 bg-white rounded-lg shadow-md mb-4 md:mb-0'>
+            <div className=' mx-5 flex flex-col md:flex-row'>
+                <div className='h-[70%] w-full md:w-1/4 p-4 bg-white rounded-lg shadow-md mb-4 md:mb-0'>
                     <FilteringProducts products={products} setFilteredProducts={setFilteredProducts} />
                 </div>
-                <div className='w-full md:w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 max-w-7xl mx-auto'>
+                <div className='m-2'>
+
+                </div>
+                <div className=' w-full md:w-3/4 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 max-w-7xl '>
                     {filteredProducts.map((product) => (
                         <Product product={product} key={product.id} />
                     ))}
